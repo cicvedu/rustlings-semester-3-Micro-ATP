e83ea7802f98a9eb247843500ca6bc7c5df789e4
陆岩淅 <131521928+Micro-ATP@users.noreply.github.com>
2024-03-12 14:07:29 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,20 +3,22 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I'm not done!
+
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    String::from(input.trim)
+    String::from(input.trim())
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
+    input.to_string() + " world!"
     
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
+    input.replace("cars", "balloons")
     
 }
 
